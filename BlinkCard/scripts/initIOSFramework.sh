3ac096b2be9d5f13b4359790e443ace3eec6aa06
mparadina <milan.paradina@microblink.com>
2024-02-23 12:40:53 +0100

    initIOSFramework script update

--- BlinkCard/scripts/initIOSFramework.sh
+++ BlinkCard/scripts/initIOSFramework.sh
@@ -4,12 +4,19 @@
 HERE="$(dirname "$(test -L "$0" && readlink "$0" || echo "$0")")"
 pushd "${HERE}/../src/ios/" > /dev/null
 
-LINK='https://github.com/BlinkCard/blinkcard-ios/releases/download/v2.6.0/BlinkCard.xcframework.zip'
+LINK='https://github.com/BlinkCard/blinkcard-ios/releases/download/v2.9.0/BlinkCard.xcframework.zip'
 FILENAME='BlinkCard.xcframework.zip'
 
-# check if BlinkCard framework and bundle already exist
-wget --version > /dev/null 2>&1 || { echo "ERROR: couldn't download BlinkCard framework, install wget" &&  exit 1; }
-wget -O "${FILENAME}" "${LINK}" -nv --show-progress || ( echo "ERROR: couldn't download BlinkCard framework, Something went wrong while downloading framework from ${LINK}" && exit 1 )
+# BlinkCard framework will be obtained via wget or curl
+if which wget >/dev/null ; then
+    echo "Downloading BlinkCard framework via wget:"
+    wget -O "${FILENAME}" "${LINK}" -nv --show-progress || ( echo "ERROR: couldn't download BlinkCard framework, something went wrong while downloading framework from ${LINK}" && exit 1 )
+elif which curl >/dev/null ; then
+    echo "Downloading BlinkCard framework via curl:"
+    curl -o "${FILENAME}" -L "${LINK}" --progress-bar --show-error || ( echo "ERROR: couldn't download BlinkCard framework, something went wrong while downloading framework from ${LINK}" && exit 1 )
+else
+    echo "Couldn't download BlinkID framework, neither wget nor curl is available."
+fi
 
 if [ -d 'Microblink.bundle' ] ; then
     rm -rf Microblink.bundle && echo "Removing Microblink.bundle"
